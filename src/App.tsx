41abab8c8c7324395962232ed850506d5edf63d9
Vitaliy Obyedov <obyedov@gmail.com>
2022-08-28 01:14:00 +0200

    Completed content from 10-11 videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header";
-import Technologies from "./components/Technologies";
-//  Hello, samurai! Let's go!
-function App() {
-  return (
-    <div className="App">
-        <Header />
-        <Technologies/>
-    </div>
 
-  );
+function App() {
+    return (
+        <div className="app-wrapper">
+            <header className={'header'}>
+                <img src="https://ershisan99.github.io/student-notes/favicon/favicon-32x32.png" alt="Samurai"/>
+            </header>
+            <nav className={'nav'}>
+                <ul>
+                    <li><a href="#/">Profile</a></li>
+                    <li><a href="#/">Messages</a></li>
+                    <li><a href="#/">News</a></li>
+                    <li><a href="#/">Music</a></li>
+                    <li><a href="#/">Settings</a></li>
+                </ul>
+            </nav>
+            <div className={'content'}>
+                <div>
+                    <img src="https://wc.wallpaperuse.com/wallp/97-975933_s.jpg" alt="img content"/>
+                </div>
+                <div>
+                    <img src="https://www.cure-naturali.it/.imaging/default/dam/cure-naturali/enciclopedia-naturale/animali/gatto-persiano.jpg/jcr:content.jpg" alt="avatar"/>
+                    + desriptions
+                </div>
+                <div>
+                    my posts
+                    <div>new post</div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
