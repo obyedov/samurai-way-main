29c7016e798691b38f90c734e28a152795f8fdf8
Vitaliy Obyedov <obyedov@gmail.com>
2022-08-28 11:53:08 +0200

    Completed content from 12-13 videos

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img src="https://ershisan99.github.io/student-notes/favicon/favicon-32x32.png" alt="Samurai"/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img src="https://wc.wallpaperuse.com/wallp/97-975933_s.jpg" alt="img content"/>
+            </div>
+            <div>
+                <img
+                    src="https://www.cure-naturali.it/.imaging/default/dam/cure-naturali/enciclopedia-naturale/animali/gatto-persiano.jpg/jcr:content.jpg"
+                    alt="avatar"/>
+                + desriptions
+            </div>
+            <div>
+                my posts
+                <div>new post</div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
